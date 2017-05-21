379d064697ff4c845f8a5f6a11e23c0c0ea2e560
antoinefuzier <antoinefuzier@hotmail.fr>
2017-05-21 16:01:15 +0200

    Update IHM.java

--- Java/Hex/src/hex/IHM.java
+++ Java/Hex/src/hex/IHM.java
@@ -1,5 +1,5 @@
 /******************************************************************
-* Menu                                                   		  *
+* IHM                                                    		  *
 * Description: Module pour jouer au jeu HEX entre deux joueurs    *
 * F. ANTOINE - Univ. de Toulouse III - Paul Sabatier              *
 *******************************************************************/
@@ -22,9 +22,10 @@ public class IHM {
 		
 		int choix=-1;
 		char pion='b';
+		String difficulte="null";
 		boolean ia=false;
 		boolean commence_ia=false;
-		String graphe="null";
+		String graphe=null;
 		int taille=0;
 		GererErreur g=new GererErreur();
 		Jouer j=new Jouer(1,1,1);
@@ -46,13 +47,15 @@ public class IHM {
 				taille=g.gererErreurInt();
 				taille=g.verifierIntervalle(taille, 2,20);
 				graphe= Plateau.creerGraphe(taille);
-				m.sauvegarderTemporaire(graphe,'.',0,0);
 				System.out.print("Qui commence? Les blancs ou les noirs? (b/n) :");
 				pion=g.gererErreurChar('b','n');
 				ia=g.saisirIA();
-				if(ia)
+				if(ia){
+					difficulte=g.choisirDiffculte();
 					commence_ia=g.commenceIA();
-				choix=j.jouerPartie(graphe,pion,ia,commence_ia);
+				}
+				m.sauvegarderTemporaire(graphe,'.',0,0,ia,commence_ia,pion,difficulte.charAt(0));
+				choix=j.jouerPartie(graphe,pion,ia,commence_ia,difficulte.charAt(0));
 				if(choix==-2){ /* -2 signifie que la partie est fini, on efface le graphe */
 					graphe="null";
 					choix=-1;
@@ -89,6 +92,7 @@ public class IHM {
 			
 			/* Charger partie */
 			else if(choix==3){
+				
 				graphe=m.charger();
 				pion=m.chargerDernierPion();
 				if(graphe=="null"){ /*Aucune partie chargée */
@@ -97,22 +101,30 @@ public class IHM {
 					choix=-1;
 				}
 				else{
-					m.sauvegarderTemporaire(graphe,'a',0,0);
+					m.sauvegarderTemporaire(graphe,'a',0,0,true,true,'.','.');
 					taille=m.recupererTaille();
 					pion=m.chargerDernierPion();
 					if(pion=='.'){	/*Chargement d'une partie non commencé */
 						m.initialisernbCoup(j);
 						System.out.print("Qui commence? Les blancs ou les noirs? (b/n) :");
 						pion=g.gererErreurChar('b','n');
-						choix=j.jouerPartie(graphe,pion,ia,commence_ia);
+						choix=j.jouerPartie(graphe,pion,ia,commence_ia,'.');
 					}
 					else if(pion=='b'){ /*Chargement d'une partie sauvegarder d'en sauvegarde.txt */
 						m.nbCoup(j);
-						choix=j.jouerPartie(graphe,'n',ia,commence_ia);
+						difficulte=m.recupererDifficulteIA();
+						if(m.recupererPionIA()=='b')
+							choix=j.jouerPartie(graphe,'n',m.recupererJouerContreIA(),false, difficulte.charAt(0));
+						else
+							choix=j.jouerPartie(graphe,'n',m.recupererJouerContreIA(),true,difficulte.charAt(0));
 					}
 					else{
 						m.nbCoup(j);
-						choix=j.jouerPartie(graphe,'b',ia,commence_ia);
+						difficulte=m.recupererDifficulteIA();
+						if(m.recupererPionIA()=='b')
+							choix=j.jouerPartie(graphe,'b',m.recupererJouerContreIA(),false,difficulte.charAt(0));
+						else
+							choix=j.jouerPartie(graphe,'b',m.recupererJouerContreIA(),true,difficulte.charAt(0));
 					}
 				}
 				
@@ -124,10 +136,17 @@ public class IHM {
 				int taille_test=m.recupererTaille();
 				int x=m.recuperDernierCoupX();				
 				int y=m.recuperDernierCoupY();				
-				if(x<0 || x>taille_test-1 || y<0 || y>taille_test-1){
+				if(x<0 || x>taille_test-1 || y<0 || y>taille_test-1 ){
 					System.out.println("Impossible de supprimer coup, pas de coup joué.");
 					System.out.println("retour au menu...");
 					choix=-1;
+				}
+				/* si un coup joué par l'ia mais pas par un joueur on ne peut pas annuler le coup */
+				else if(m.recupererJouerContreIA() && m.recupererIACommence() && m.nombreDeCoupJouer()<=1){
+					System.out.println("Impossible de supprimer coup, pas de coup joué.");
+					System.out.println("retour au menu...1");
+					choix=-1;
+					
 				}
 				else if(j.gagnant(m.chargerPlateauTemp())!='.'){ 
 					System.out.println("Impossible d'annuler le dernier coup, la partie est fini");
@@ -135,16 +154,71 @@ public class IHM {
 					choix=-1;
 					
 				}
+				
 				else{
-					graphe=Plateau.annulerDernierCoup(graphe,x,y);
+					
+					/*graphe=Plateau.annulerDernierCoup(graphe,x,y);
 					pion=m.chargerDernierPion();
-					if(pion=='b'){
-						m.nbCoup(j);
-						choix=j.jouerPartie(graphe,'b',ia,commence_ia);
+					m.effacerDernierPion();
+					*/
+					if(m.recupererJouerContreIA()){ /*si on joue contre l'ia */
+						
+						if(m.recupererIACommence()){ /*si l'ia a commencé la partie */
+						
+							/*on recupere son dernier coup dans le fichier */
+							x=m.recuperDernierCoupX();				
+							y=m.recuperDernierCoupY();
+							/*on annule le coup dans le graphe */
+							graphe=Plateau.annulerDernierCoup(graphe,x,y);
+							/*on l'efface dans le fichier*/
+							m.effacerDernierPion();
+							/*on recupere le dernier coup du joueur et le pion */
+							x=m.recuperDernierCoupX();				
+							y=m.recuperDernierCoupY();
+							pion=m.chargerDernierPion();
+							/* on annule le coup du joueur dans le graphe */
+							graphe=Plateau.annulerDernierCoup(graphe,x,y);
+							m.effacerDernierPion();
+							/*le joueur joue son coup */
+							if(pion=='b'){
+								m.nbCoup(j);
+								choix=j.jouerPartie(graphe,'b',ia,false,m.recupererDifficulteIA().charAt(0));
+							}
+							else{
+								m.nbCoup(j);
+								choix=j.jouerPartie(graphe,'n',ia,false,m.recupererDifficulteIA().charAt(0));
+							}
+					
+						}
+						else{ /*si c'est le joueur qui commence */
+							x=m.recuperDernierCoupX();				
+							y=m.recuperDernierCoupY();
+							graphe=Plateau.annulerDernierCoup(graphe,x,y);
+							pion=m.chargerDernierPion();
+							m.effacerDernierPion();
+							if(pion=='b'){
+								m.nbCoup(j);
+								choix=j.jouerPartie(graphe,'b',ia,false,'.');
+							}
+							else{
+								m.nbCoup(j);
+								choix=j.jouerPartie(graphe,'n',false,false,'.');
+							}
+							
+						}
 					}
 					else{
-						m.nbCoup(j);
-						choix=j.jouerPartie(graphe,'n',ia,commence_ia);
+						graphe=Plateau.annulerDernierCoup(graphe,x,y);
+						pion=m.chargerDernierPion();
+						m.effacerDernierPion();
+						if(pion=='b'){
+							m.nbCoup(j);
+							choix=j.jouerPartie(graphe,'b',false,false,'.');
+						}
+						else{
+							m.nbCoup(j);
+							choix=j.jouerPartie(graphe,'n',false,false,'.');
+						}
 					}
 				}
 			}
